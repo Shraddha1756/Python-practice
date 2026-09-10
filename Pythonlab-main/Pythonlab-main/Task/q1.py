c171af459f411305dfb06906a7c4f19194da512a
Shraddha1756 <2024a1r034@mietjammu.in>
2026-09-10 11:49:55 +0530

    Create q1.py

---
+++ Pythonlab-main/Pythonlab-main/Task/q1.py
@@ -0,0 +1,11 @@
+'''Write a python program to input four number  from the user and find the greatest among them'''
+#solution
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+d = int(input("Enter fourth number: "))
+
+greatest = max(a, b, c, d)
+
+print("Greatest number is:", greatest)
