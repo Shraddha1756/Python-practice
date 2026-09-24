06649548c2cf52a7fa2a34f829205dc2d1043825
Shraddha1756 <2024a1r034@mietjammu.in>
2026-09-24 11:55:34 +0530

    Create q3.py

---
+++ Pythonlab-main/Pythonlab-main/task15/q3.py
@@ -0,0 +1,13 @@
+''' write a python program to input number in a list and find the second largest  number.
+'''
+n = int(input("Enter number of elements: "))
+
+numbers = []
+
+for i in range(n):
+    num = int(input("Enter number: "))
+    numbers.append(num)
+
+numbers.sort()
+
+print("Second largest number:", numbers[-2])
