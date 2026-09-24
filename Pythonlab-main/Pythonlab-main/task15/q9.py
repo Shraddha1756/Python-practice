b846dd477023133ec5bb6ca53f1532d85d8ab0ca
Shraddha1756 <2024a1r034@mietjammu.in>
2026-09-24 12:11:45 +0530

    Create q9.py

---
+++ Pythonlab-main/Pythonlab-main/task15/q9.py
@@ -0,0 +1,7 @@
+lst = list(map(int, input("Enter list elements: ").split()))
+
+element = int(input("Enter the element to count: "))
+
+count = lst.count(element)
+
+print("The element appears", count, "times.")'''
